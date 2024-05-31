9281b05eaa6ec1eefb9d0af259997ff4834a19c0
BulhakovOzero <56506157+AndriiBulhakov@users.noreply.github.com>
2024-05-31 15:48:33 +0200

    converted images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -98,14 +98,14 @@ const Hero = () => {
           />
           <div className="absolute z-[-1] w-[270.259%] h-[250.8%] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex items-center justify-center blur">
             <Image
-              src="/assets/images/hero-video-bg-1.svg"
+              src="/assets/images/hero-video-bg-1.webp"
               alt="Hero"
               className="hero-star w-[66.507%] absolute h-[66.507%]"
               width={627}
               height={627}
             />
             <Image
-              src="/assets/images/hero-video-bg-2.svg"
+              src="/assets/images/hero-video-bg-2.webp"
               alt="Hero"
               className="hero-star w-full h-full"
               width={417}
